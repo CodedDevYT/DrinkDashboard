36bfebf93ea20a77f64102071211ecb6273f6f24
unknown <ytcodecrafter@gmail.com>
2023-09-02 16:08:23 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,31 +8,38 @@ export default function Home() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="container">
+
+    <div className="{styles.container}">
       <h1>Drink Dashboard</h1>
-      <div className="drink-option">
-        <span className="drink-name">Cola / Frucade</span>
-        <span className="drink-price">3.50€</span>
-        <button className="select-button" onClick={() => setCount(count + 3.50)}>Select</button>
+      <div className="{styles.drink-option}">
+        <span className="{styles.drink-name}">Cola / Frucade</span>
+        <span className="{styles.drink-price}">3.50€</span>
+        <button className="{styles.select-button}" onClick={() => setCount(count + 3.50)}>Select</button>
       </div>
-      <div className="drink-option">
-        <span className="drink-name">Spezi</span>
-        <span className="drink-price">2.50€</span>
-        <button className="select-button" onClick={() => setCount(count + 2.50)}>Select</button>
+      <div className="{styles.drink-option}">
+        <span className="{styles.drink-name}">Spezi</span>
+        <span className="{styles.drink-price}">2.50€</span>
+        <button className="{styles.select-button}" onClick={() => setCount(count + 2.50)}>Select</button>
       </div>
-      <div className="drink-option">
-        <span className="drink-name">Apfelschorle</span>
-        <span className="drink-price">3.00€</span>
-        <button className="select-button" onClick={() => setCount(count + 3.00)}>Select</button>
+      <div className="{styles.drink-option}">
+        <span className="{styles.drink-name}">Apfelschorle</span>
+        <span className="{styles.drink-price}">3.00€</span>
+        <button className="{styles.select-button}" onClick={() => setCount(count + 3.00)}>Select</button>
       </div>
-      <div className="drink-option">
-        <span className="drink-name">Sprudel</span>
-        <span className="drink-price">2.50€</span>
-        <button className="select-button" onClick={() => setCount(count + 2.50)}>Select</button>
+      <div className="{styles.drink-option}">
+        <span className="{styles.drink-name}">Sprudel</span>
+        <span className="{styles.drink-price}">2.50€</span>
+        <button className="{styles.select-button}" onClick={() => setCount(count + 2.50)}>Select</button>
       </div>
-      <div className="total-price">
-        Total Price: <span id="totalPrice">{count}€</span>
+      <div className="{styles.drink-option}">
+        <span className="{styles.drink-name}">Pfand</span>
+        <span className="{styles.drink-price}">2.00€</span>
+        <button className="{styles.select-button}" onClick={() => setCount(count + 2.00)}>Select</button>
       </div>
+      <div className="{styles.total-price}">
+        Total Price: <span id="{styles.totalPrice}">{count}€</span>
+      </div>
+      <button className="{styles.select-button}" onClick={() => setCount(0)}>Reset</button>
     </div>
   )
 }
