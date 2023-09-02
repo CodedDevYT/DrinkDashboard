d9acab6c83ac423d6438839983b3ab9f6fa29c82
unknown <ytcodecrafter@gmail.com>
2023-09-02 16:32:35 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,46 +11,56 @@ export default function Home() {
 
     <div className="container">
       <h1>Drink Dashboard</h1>
-      <div className="drink-option">
-        <span className="drink-name">Flasche Wein 1L</span>
-        <span className="drink-price">13€</span>
-        <button className="select-button" onClick={() => setCount(count + 13)}>Add</button>
+
+      <div className="option-1">
+        <div className="drink-option">
+          <span className="drink-name">Flasche Wein 1L</span>
+          <span className="drink-price">13€</span>
+          <button className="select-button" onClick={() => setCount(count + 13)}>Add</button>
+        </div>
+        <div className="drink-option">
+          <span className="drink-name">Wein 1/4 L</span>
+          <span className="drink-price">3.50€</span>
+          <button className="select-button" onClick={() => setCount(count + 3.50)}>Add</button>
+        </div>
+        <div className="drink-option">
+          <span className="drink-name">Schorle 1/4 L</span>
+          <span className="drink-price">3.00€</span>
+          <button className="select-button" onClick={() => setCount(count + 3.00)}>Add</button>
+        </div>
+        <div className="drink-option">
+          <span className="drink-name">Cola / Frucade 0,33 L</span>
+          <span className="drink-price">2.50€</span>
+          <button className="select-button" onClick={() => setCount(count + 2.50)}>Add</button>
+        </div>
+        <div className="drink-option">
+          <span className="drink-name">Spezi 0.5L</span>
+          <span className="drink-price">3.00€</span>
+          <button className="select-button" onClick={() => setCount(count + 3.00)}>Add</button>
+        </div>
       </div>
-      <div className="drink-option">
-        <span className="drink-name">Wein 1/4 L</span>
-        <span className="drink-price">3.50€</span>
-        <button className="select-button" onClick={() => setCount(count + 3.50)}>Add</button>
+
+      <div className="option-2">
+
+        <div className="drink-option">
+          <span className="drink-name">Apfelsaft Schorle 0.5L</span>
+          <span className="drink-price">3.00€</span>
+          <button className="select-button" onClick={() => setCount(count + 3.00)}>Add</button>
+        </div>
+        <div className="drink-option">
+          <span className="drink-name">Sprudel 0.5L süß / sauer</span>
+          <span className="drink-price">2.50€</span>
+          <button className="select-button" onClick={() => setCount(count + 2.50)}>Add</button>
+        </div>
+        <div className="drink-option">
+          <span className="drink-name">Pfand</span>
+          <span className="drink-price">2.00€</span>
+          <button className="select-button" onClick={() => setCount(count + 2.00)}>Add</button>
+        </div>
+
       </div>
-      <div className="drink-option">
-        <span className="drink-name">Schorle 1/4 L</span>
-        <span className="drink-price">3.00€</span>
-        <button className="select-button" onClick={() => setCount(count + 3.00)}>Add</button>
-      </div>
-      <div className="drink-option">
-        <span className="drink-name">Cola / Frucade 0,33 L</span>
-        <span className="drink-price">2.50€</span>
-        <button className="select-button" onClick={() => setCount(count + 2.50)}>Add</button>
-      </div>
-      <div className="drink-option">
-        <span className="drink-name">Spezi 0.5L</span>
-        <span className="drink-price">3.00€</span>
-        <button className="select-button" onClick={() => setCount(count + 3.00)}>Add</button>
-      </div>
-      <div className="drink-option">
-        <span className="drink-name">Apfelsaft Schorle 0.5L</span>
-        <span className="drink-price">3.00€</span>
-        <button className="select-button" onClick={() => setCount(count + 3.00)}>Add</button>
-      </div>
-      <div className="drink-option">
-        <span className="drink-name">Sprudel 0.5L süß / sauer</span>
-        <span className="drink-price">2.50€</span>
-        <button className="select-button" onClick={() => setCount(count + 2.50)}>Add</button>
-      </div>
-      <div className="drink-option">
-        <span className="drink-name">Pfand</span>
-        <span className="drink-price">2.00€</span>
-        <button className="select-button" onClick={() => setCount(count + 2.00)}>Add</button>
-      </div>
+
+
       <div className="total-price">
         Total Price: <span id="totalPrice">{count}€</span>
       </div>
