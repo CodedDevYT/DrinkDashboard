5c3a9aac7060d25598d437dbdafb95cc03e3b981
peu77 <emilebert@icloud.com>
2023-09-03 12:10:23 +0200

    made it more user-friendly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,43 @@
 'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
-import {useState} from 'react';
+import {useMemo, useState} from 'react';
+
+interface Drink {
+    name: string,
+    price: number
+}
 
 export default function Home() {
 
-    const [count, setCount] = useState(0)
-    const [list, setList] = useState("")
+    const [list, setList] = useState([] as Drink[])
 
-    const drinks = [
+    const price = useMemo(() => {
+        let totalPrice = 0
+        list.forEach((item) => {
+            totalPrice += item.price
+        })
+        return totalPrice
+    }, [list])
+
+    function getAmountOfDrink(name: string): number {
+        let amount = 0
+        list.forEach((item) => {
+            if (item.name === name) {
+                amount++
+            }
+        })
+        return amount
+    }
+
+    function formatPriceToEuro(price: number): string {
+        return price.toFixed(2).replace('.', ',')
+    }
+
+    const drinks: Drink[] = [
         {
             name: "Flasche Wein 1L",
-            price: 13
+            price: 13,
         },
         {
             name: "Wein 1/4 L",
@@ -45,30 +71,38 @@ export default function Home() {
 
     return (
 
-        <div className="container">
-            <h1>Drink Dashboard</h1>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Drink Dashboard</h1>
 
-            <div className="itemContainer">
+            <div className={styles.itemContainer}>
                 {drinks.map((drink, index) => (
-                    <div className="drink" key={index} onClick={() => {
-                        setCount(count + drink.price)
-                        setList(list + drink.name + " & ")
+                    <div className={styles.drink} key={index} onClick={() => {
+                        setList([...list, drink])
                     }}>
-                        <span className="drink-name">{drink.name}</span>
-                        <span className="drink-price">{drink.price}€</span>
+                        <span className={styles.drinkName}>{drink.name}</span>
+                        <span className={styles.drinkAmount}>{getAmountOfDrink(drink.name)}x</span>
+                        <span className={styles.drinkPrice}>{drink.price}€</span>
 
                     </div>
                 ))}
             </div>
 
-            <p className='items'>{list}</p>
-
-            <div className="total-price">
-                Total Price: <span id="totalPrice">{count}€</span>
+            <div className={styles.selectedDrinks}>
+                {list.map((drink, index) => (
+                    <div className={styles.selectedDrink} key={index} onClick={() => {
+                        setList(list.filter((item) => item.name !== drink.name))
+                    }}>
+                        <span className={styles.name}>{drink.name}</span>
+                        <span className={styles.price}>{drink.price}€</span>
+                    </div>
+                ))}
             </div>
-            <button className="select-button" onClick={() => {
-                setCount(0)
-                setList("")
+
+            <div className={styles.totalPrice}>
+                Total Price: <span id="totalPrice">{formatPriceToEuro(price)}€</span>
+            </div>
+            <button className={styles.resetButton} onClick={() => {
+                setList([])
             }}>Reset
             </button>
         </div>
